99d2c1dfe171dc3b1a1d12968f3f87ea76b0627b
ssm-0123 <ssm-0123@users.noreply.github.com>
2021-09-27 14:28:11 -0700

    gooo

--- problem2.py
+++ problem2.py
@@ -26,3 +26,21 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+
+xx = input("xx?:")
+yy = input("yy?:")
+xx = int(xx)
+yy = int(yy)
+
+if xx >= yy :
+    if xx%yy == 0 :
+        print(yy,"is a factor of",xx)
+    elif xx%yy != 0 :
+        print(yy,"is not a factor of",xx)
+
+if yy >= xx :
+    if yy%xx == 0 :
+        print(xx,"is a factor of",yy)
+    elif yy%xx != 0 :
+        print(xx,"is not a factor of",yy)
+
--- problem3.py
+++ problem3.py
@@ -30,3 +30,41 @@ Enter an integer=>4
 Enter an integer=>2
 2,4,5 do not form a Pythagorean triple
 """
+
+n1 = input("first integer")
+n2 = input("second integer")
+n3 = input("third integer")
+
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+
+big = max(n1,n2,n3)
+sma = min(n1,n2,n3)
+if n1 == big and n2 == sma:
+    mid = n3
+if n2 == big and n3 == sma:
+    mid = n1
+if n3 == big and n1 == sma:
+    mid = n2
+if n1 == big and n3 == sma:
+    mid = n2
+if n2 == big and n1 == sma:
+    mid = n3
+if n3 == big and n2 == sma:
+    mid = n1
+
+
+
+if big**2 == mid**2+sma**2 :
+    sma = str(sma)
+    mid = str(mid)
+    big = str(big)
+    print(sma+","+mid+","+big,"form a Pythagorean triple")
+else:
+    sma = str(sma)
+    mid = str(mid)
+    big = str(big)
+    print(sma+","+mid+","+big,"do not form a Pythagorean triple")
+
+
